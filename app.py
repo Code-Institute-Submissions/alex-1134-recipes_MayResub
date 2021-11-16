ad681d8c8a60f475c5335bae44925d2eeef0e1b3
alex-1134 <alexa.t.lach@gmail.com>
2021-11-16 14:14:14 +0000

    Add User Profile

--- app.py
+++ app.py
@@ -42,9 +42,10 @@ def register():
         }
         mongo.db.users.insert_one(register)
 
-        # put new user into seshion cookie
+        # put new user into session cookie
         session["user"] = request.form.get("username").lower()
         flash("Congrats! You're in!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -60,7 +61,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 # in case of invalid password
                 flash("Incorrect Username or/and Password")
@@ -72,6 +76,15 @@ def login():
             return redirect(url_for("login"))
             
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # get the session user's name from the database
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
