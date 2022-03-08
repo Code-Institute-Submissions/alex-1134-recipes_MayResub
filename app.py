fd18b8c378cbda5b21c0d366744cca56a9001edb
alex-1134 <alexa.t.lach@gmail.com>
2022-03-08 09:21:13 +0000

    Fix delete functionality

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def edit_recipe(recipe_id):
 
 @app.route("/delete_recipe/<recipe_id>")
 def delete_recipe(recipe_id):
-    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    mongo.db.recipes.delete_one({"_id": ObjectId(recipe_id)})
     flash("Recipe Successfully Deleted")
     return redirect(url_for("get_recipes"))
 
